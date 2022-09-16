1e5180e6eccd13c72b4ddd6df52a4f8d989e7658
Masenkyo <33390@ma-web.nl>
2022-09-16 21:17:03 +0200

    lopen gefixt, highland tile, gate hp, enemy dmg, elemental enemy.

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -7,22 +7,16 @@ using UnityEngine.UIElements;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField]private float Speed;
-
-    void Start()
-    {
-        
-    }
-
-
+    int i = 0;
+    Vector3[] array = new Vector3[] { (new Vector3(-0.5f, 1.5f)), (new Vector3(-6.5f, 1.5f)), (new Vector3(-6.5f, -2.5f)), (new Vector3(1.5f, -2.5f)), (new Vector3(1.5f, -0.5f)), (new Vector3(3.5f, -0.5f)), (new Vector3(3.5f, 3.5f)), (new Vector3(7.5f, 3.5f)), (new Vector3(7.5f, -2.5f)), (new Vector3(5.5f, -2.5f)), (new Vector3(5.5f, -5f)) };
     void Update()
     {
-
-        transform.position -= transform.up * Speed * Time.deltaTime;
-
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        transform.Rotate(transform.position.x, 0, transform.position.z);
+        Vector3 Direction = array[i] - transform.position;
+        Direction /= Direction.magnitude;
+        transform.position += Time.deltaTime * Direction;
+        if ((array[i] - transform.position).magnitude < 0.01f)
+        {
+            i++;
+        }
     }
 }
